880578b71816417b6e1a9523c3f7945387c268d3
adcalderonc <112212138+adcalderonc@users.noreply.github.com>
2022-10-18 23:02:01 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -42,9 +42,8 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --
 SELECT
-   c23,
-   avg(c21)
+  strftime('%Y' , c23), avg(c21)
 FROM
     tbl2
 GROUP BY 
-    c23
+  strftime('%Y' , c23)
